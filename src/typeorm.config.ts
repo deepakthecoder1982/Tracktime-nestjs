c416e34f79e3bc3a08a1d00df4b5ac0b2afb7a78
shiv <developer.shivanshu@gmail.com>
2024-02-16 13:30:53 +0530

    Work

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config(); // Load environment variables from .env files
 
 const dbUrl = process.env.DATABASE_URL;
 
