4199073dd5ccbc19895437c0bc359f724ce02964
Deepak Yadav <108220666+deepakthecoder1982@users.noreply.github.com>
2026-05-06 08:32:59 +0000

    flask_api

--- src/organisation/onboarding.service.ts
+++ src/organisation/onboarding.service.ts
@@ -129,7 +129,7 @@ const weekdayData = [
 // You can then save this `weekdayData` into the database using your existing repository methods.
 
 export let DeployFlaskBaseApi =
-  'https://tracktime-flask-ai-zy26.onrender.com';
+  'https://tracktime-flask-ai-latest-version.onrender.com';
 export let LocalFlaskBaseApi = 'http://127.0.0.1:5000';
 // DeployFlaskBaseApi=LocalFlaskBaseApi;
 type UpdateConfigType = DeepPartial<User['config']>;
